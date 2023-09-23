3b3233434e2dc0d4645aeb03f56df4e68654731b
Roee Shoshani <roysho10@gmail.com>
2023-09-23 15:17:07 +0300

    remove the example used for testing

--- examples/basic.rs
+++
@@ -1,7 +0,0 @@
-use recursive_array::RecursiveArray;
-
-fn main() {
-    let array = recursive_array::recursive_array![1, 2, 3];
-    let array2 = array.push_back(4);
-    println!("{:?}", array2.as_slice())
-}
